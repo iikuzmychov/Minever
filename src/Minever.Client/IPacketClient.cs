e872dd1e7e514437b5cf506ae72e2db9b9533260
kuzcode <igormakw228@gmail.com>
2022-09-04 17:45:55 +0300

    Update IPacketClient

--- src/Minever.Client/IPacketClient.cs
+++ src/Minever.Client/IPacketClient.cs
@@ -6,11 +6,18 @@
     //public abstract static Task<(TimeSpan Ping, ... Status)> PingServerAsync();
 
     public ValueTask ConnectAsync(string host, ushort port, CancellationToken cancellationToken = default);
-    public Action<object> OnPacket<TData>(Action<TData> action);
-    public void OnceOnPacket<TData>(Action<TData> action);
-    public Task<TData> WaitPacketAsync<TData>(CancellationToken cancellationToken = default);
+    
+    public Action<object> OnPacket<TData>(Action<TData> action) where TData : notnull;
+    
+    public void OnceOnPacket<TData>(Action<TData> action) where TData : notnull;
+    
+    public Task<TData> WaitPacketAsync<TData>(CancellationToken cancellationToken = default) where TData : notnull;
+    
     public void SendPacket(object packetData);
-    public Task<TResponseData> SendRequestAsync<TResponseData>(object requestPacketData, CancellationToken cancellationToken = default);
+
+    public Task<TResponseData> SendRequestAsync<TResponseData>(
+        object requestPacketData, CancellationToken cancellationToken = default)
+        where TResponseData : notnull;
 }
 
 // IProtocol protocol         = new JavaProtocolX(); // or BedrockProtocolX
