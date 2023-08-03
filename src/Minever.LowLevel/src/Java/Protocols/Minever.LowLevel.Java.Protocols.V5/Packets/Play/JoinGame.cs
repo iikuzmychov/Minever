9574d6d5bb73edf3e927d07c94e0517e29405c37
Ihor Kuzmychov <iikuzmychov@gmail.com>
2023-08-04 01:12:31 +0200

    Fix JoinGame packet

--- src/Minever.LowLevel/src/Java/Protocols/Minever.LowLevel.Java.Protocols.V5/Packets/Play/JoinGame.cs
+++ src/Minever.LowLevel/src/Java/Protocols/Minever.LowLevel.Java.Protocols.V5/Packets/Play/JoinGame.cs
@@ -1,24 +1,42 @@
 ﻿using Minever.LowLevel.Core.Packets.Serialization.Attributes;
 using Minever.LowLevel.Core.Packets.Serialization.Converters;
+using Minever.LowLevel.Java.Core;
 using Minever.LowLevel.Java.Protocols.V5.Enums;
 
 namespace Minever.LowLevel.Java.Protocols.V5.Packets.Play;
 
+/// <summary>
+/// Id: 0x01 <br/>
+/// State: <see cref="JavaConnectionState.Play"/> <br/>
+/// Direction: <see cref="PacketDirection.FromServer"/> <br/>
+/// <br/>
+/// See <see href="https://wiki.vg/index.php?title=Protocol&amp;oldid=6003#Join_Game">Play/Clientbound/JoinGame</see> on wiki.vg.
+/// </summary>
+[JavaPacket<JavaProtocol5>(0x01, JavaConnectionState.Play, PacketDirection.FromServer)]
 public sealed record JoinGame
 {
+    private string _levelType = default!;
+
     [PacketPropertyOrder(1)]
-    public int PlayerEntityId { get; init; }
+    public required int PlayerEntityId { get; init; }
 
     [PacketPropertyOrder(2)]
-    public Gamemode Gamemode { get; init; }
+    public required Gamemode Gamemode { get; init; }
 
     [PacketPropertyOrder(3)]
     [PacketConverter<PacketEnumConverter<Dimension, sbyte>>]
-    public Dimension Dimension { get; init; }
+    public required Dimension Dimension { get; init; }
 
     [PacketPropertyOrder(4)]
-    public Difficulty Difficulty { get; init; }
+    public required Difficulty Difficulty { get; init; }
 
     [PacketPropertyOrder(5)]
-    public byte MaxPlayersCount { get; init; }
+    public required byte MaxPlayersCount { get; init; }
+
+    [PacketPropertyOrder(6)]
+    public required string LevelType
+    {
+        get => _levelType;
+        init => _levelType = value ?? throw new ArgumentNullException(nameof(value));
+    }
 }
