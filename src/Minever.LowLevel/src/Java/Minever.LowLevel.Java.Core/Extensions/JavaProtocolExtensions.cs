eb2c64dc7e9056d90a9c4aeac5b50983be641c30
Ihor Kuzmychov <iikuzmychov@gmail.com>
2023-08-04 00:33:32 +0200

    Add todo

--- src/Minever.LowLevel/src/Java/Minever.LowLevel.Java.Core/Extensions/JavaProtocolExtensions.cs
+++ src/Minever.LowLevel/src/Java/Minever.LowLevel.Java.Core/Extensions/JavaProtocolExtensions.cs
@@ -9,6 +9,7 @@ public static class JavaProtocolExtensions
 
         if (!protocol.IsPacketSupported(packet, context))
         {
+            // todo: use different exception class
             throw new ArgumentException("The packet is not supported by the protocol in the specified context.");
         }
     }
