96ff75670869434934312b4c979ed0dc3a3a5861
bohdan <bogdanefremenko43@gmail.com>
2023-09-04 00:49:40 +0300

    Implement PlayerAbilities packet

--- src/Minever.LowLevel/src/Java/Protocols/Minever.LowLevel.Java.Protocols.V5/Packets/Play/PlayerAbilities/PlayerAbilities.cs
+++ src/Minever.LowLevel/src/Java/Protocols/Minever.LowLevel.Java.Protocols.V5/Packets/Play/PlayerAbilities/PlayerAbilities.cs
@@ -1,16 +1,28 @@
 ﻿using Minever.LowLevel.Core.Packets.Serialization.Attributes;
+using Minever.LowLevel.Java.Core;
+using Minever.LowLevel.Java.Protocols.V5.Packets.Serialization.Converters;
 
 namespace Minever.LowLevel.Java.Protocols.V5.Packets.Play.PlayerAbilities;
 
 // todo: think about naming
+/// <summary>
+/// Id: 0x39 <br/>
+/// State: <see cref="JavaConnectionState.Play"/> <br/>
+/// Direction: <see cref="PacketDirection.FromServer"/> <br/>
+/// <br/>
+/// See <see href="https://wiki.vg/index.php?title=Protocol&amp;oldid=6003#Player_Abilities">Play/Clientbound/PlayerAbilities</see> on wiki.vg.
+/// </summary>
+[JavaPacket<JavaProtocol5>(0x39, JavaConnectionState.Play, PacketDirection.FromServer)]
 public sealed record PlayerAbilities
 {
     [PacketPropertyOrder(1)]
-    public PlayerAbilitiesFlags Flags { get; init; }
+    public required PlayerAbilitiesFlags Flags { get; init; }
 
     [PacketPropertyOrder(2)]
-    public float FlyingSpeed { get; init; }
+    [PacketConverter<PacketFloat255Converter>]
+    public required float FlyingSpeed { get; init; }
 
     [PacketPropertyOrder(3)]
-    public float WalkingSpeed { get; init; }
+    [PacketConverter<PacketFloat255Converter>]
+    public required float WalkingSpeed { get; init; }
 }
---
+++ src/Minever.LowLevel/src/Java/Protocols/Minever.LowLevel.Java.Protocols.V5/Packets/Serialization/Converters/PacketFloat255Converter.cs
@@ -0,0 +1,18 @@
+﻿using System.Buffers.Binary;
+using Minever.LowLevel.Core.IO;
+using Minever.LowLevel.Core.Packets.Serialization.Converters;
+
+namespace Minever.LowLevel.Java.Protocols.V5.Packets.Serialization.Converters;
+
+public class PacketFloat255Converter : PacketConverter<float>
+{
+    public override float Read(MinecraftReader reader)
+    {
+        ArgumentNullException.ThrowIfNull(reader);
+        
+        return reader.ReadFloat() * 250f;
+    }
+
+    public override void Write(MinecraftWriter writer, float value)
+        => throw new NotImplementedException();
+}
